45a3dd68b3b849ef3ab25b4e9fce4fbeb418965b
vivek sangode <vivekusangode@gmail.com>
2022-11-11 11:26:45 +0530

    Day-3 completed

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+document.querySelector("#nav-container").addEventListener("scroll", (event) => {
+    document.querySelector("#nav-container").style.position = "fixed";
+})
+
+function nav(){
+    document.querySelector("#nav-container").style.position = "fixed";
+}
+
 var videoplayer = document.querySelector("#player");
 var myvideo = document.querySelector("#myvideo");
-function stopVideo(){
+function stopVideo() {
     videoplayer.style.display = "none";
 }
 function playVideo() {
     videoplayer.style.display = "block";
-} 
+}  
