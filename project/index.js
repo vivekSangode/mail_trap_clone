f6fd05ecb7757eaa3170d7ca3f9a1a0e4edb5a1f
Vivek Uttam Sangode <112774227+vivekSangode@users.noreply.github.com>
2022-11-11 09:25:10 +0530

    Delete project directory

--- project/index.js
+++
@@ -1,8 +0,0 @@
-var videoplayer = document.querySelector("#player");
-var myvideo = document.querySelector("#myvideo");
-function stopVideo(){
-    videoplayer.style.display = "none";
-}
-function playVideo() {
-    videoplayer.style.display = "block";
-} 
